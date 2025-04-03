829ee3847c403af44cead6ba05318bf9b63cfefb
Sampuran429 <143642553+Sampuran429@users.noreply.github.com>
2025-04-04 00:01:31 +0530

    Updated frontend with new changes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
-import path from "path";
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
+  plugins: [react(),tailwindcss(),],
+   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+})
